06f07f03c2d4f3912f9f7875e77ea8f0c2aaeb1b
lide <lide1202@hotmail.com>
2018-03-04 15:48:32 +0800

    add error log in exception resolver

--- src/main/java/com/a7space/commons/authority/resolver/MVCExceptionResolver.java
+++ src/main/java/com/a7space/commons/authority/resolver/MVCExceptionResolver.java
@@ -3,6 +3,8 @@ package com.a7space.commons.authority.resolver;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,11 +14,14 @@ import com.a7space.commons.exception.PermissionDeniedException;
 import com.a7space.commons.exception.ServiceException;
 
 public class MVCExceptionResolver implements HandlerExceptionResolver{
-
+	
+	private final Logger logger=LoggerFactory.getLogger(getClass());
+	
 	@Override
 	public ModelAndView resolveException(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2,
 			Exception ex) {
 		// TODO Auto-generated method stub
+		logger.error("error:{}",ex);
 		ModelAndView  mv=new ModelAndView("redirect:/err500.jsp");
 		
 		if(ex instanceof NoLoginException) {
